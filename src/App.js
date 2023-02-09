0c8eea3294c1b83fe36ae7e22de827d37f72fde9
Wooseong Kim <kimwooseong8313@gmail.com>
2023-02-09 23:37:12 +0900

    feat: 페이지 경로 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import MainPage from "./pages/MainPage";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Todo from "./pages/Todo";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+`;
